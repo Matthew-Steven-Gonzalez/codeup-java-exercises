631f8aa1b40771ae62f8da4cc5494bc96400dfe2
Matthew <gonzalez.matthew.steven@gmail.com>
2023-01-11 10:17:54 -0500

    corrected data type from int to double

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,19 +1,16 @@
 package shapes;
 
 public class Square extends Rectangle {
-    public Square(int length, int width) {
-        super(length, width);
-    }
 
-    public Square( int side){
+    public Square( double side){
         super(side, side);
     }
 
-    public int getArea(){
+    public double getArea(){
         return (int) Math.pow(this.length,2);
     }
 
-    public int getPerimeter(){
+    public double getPerimeter(){
         return this.width * 4;
     }
 }
