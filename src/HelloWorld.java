5205398e2bfd63c4022534da2d728191b20ce880
Matthew <gonzalez.matthew.steven@gmail.com>
2023-01-21 16:57:52 -0500

    added missing work.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,45 @@
 public class HelloWorld {
     public static void main(String[] args) {
+
+        int myFavoriteNumber = 9;
+
+        System.out.println(myFavoriteNumber);
+
+        String myString = "a string value";
+
+
+//        myString = a;
+
+//        myString = 3.14159;
+
+//        long myNumber;
+
+//        long myNumber= 3.14L;
+
+//        long myNumber= 3.14;
+
+//        float myNumber= (float) 3.14;
+//
+//        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//
+//        int three = (int) "three";
+
+
+        System.out.println(myString);
+
+
 //        int x = 4;
 //        x += 5;
 //        System.out.println(x);
@@ -8,12 +48,12 @@ public class HelloWorld {
 //        x *= 4;
 //        System.out.println(x);
 
-        int x = 10;
-        int y = 2;
-        x /= y;
-        System.out.println(x);
-        y -= x;
-        System.out.println(y);
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        System.out.println(x);
+//        y -= x;
+//        System.out.println(y);
     }
 }
 
